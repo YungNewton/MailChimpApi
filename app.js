af4bf1ec6ef5d8c0ee7d342eb1db0c6c5130790a
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-17 17:16:46 +0100

    commit

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const https = require('https')
 const app = express();
 const port = 8080;
 
-app.use(express.static('styles.css'));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/",(req, res)=>{
     res.sendFile(__dirname+"/sign_up.html");
 })
 
+app.post('/',(req, res)=>{
+    var email = req.body.email;
+    var firstName = req.body.firstName
+    var lastName = req.body.lastName
+    res.send("<h2 style ='font-family:Georgia, 'Times New Roman', Times, serif;'> Thank you for signing up "+ firstName +".<h2/>")
+    console.log(email+ " "+firstName+ " "+lastName)
+
+})
+
 app.listen(port, ()=>{
-    console.log("server running on port 3000");
+    console.log("server running on port "+port);
 })
