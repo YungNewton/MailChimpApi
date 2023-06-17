31bde1685a42046a68da111645ae624985ed5fa6
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-17 01:12:08 +0100

    commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const app =express();
-const port = 3000;
+const app = express();
+const port = 8080;
 
-app.listen(port);
+app.get("/",(req, res)=>{
+    res.send("Server up and running");
+})
 
-app.get('/',(req, res)=>{
-    res.send("Server up and running")
+app.listen(port, ()=>{
+    console.log("server running on port 3000");
 })
